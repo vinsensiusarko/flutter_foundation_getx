916b4f8f2a5897e8433d1658ce4dbcc94ba59668
vinsensiusarko <vinsensiusarka@gmail.com>
2026-03-19 16:51:05 +0700

    refactor: use PageController for navigation in MainController

--- lib/app/modules/main/controllers/main_controller.dart
+++ lib/app/modules/main/controllers/main_controller.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../home/views/home_view.dart';
-import '../../news/views/news_view.dart';
-import '../../setting/views/setting_view.dart';
-
 class MainController extends GetxController {
+  final PageController pageController = PageController();
   int currentIndex = 0;
 
-  final List<Widget> screenList = [
-    const HomeView(),
-    const NewsView(),
-    const SettingView(),
-  ];
+  void onSwipePageIndex(int index) {
+    currentIndex = index;
+    update();
+  }
 
   void onTapIndex(int index) {
-    currentIndex = index;
+    pageController.jumpToPage(index);
     update();
   }
 }
