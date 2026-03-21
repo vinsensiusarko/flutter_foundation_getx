2004c96d0e1086449e7216d915b02c491c260a4d
vinsensiusarko <vinsensiusarka@gmail.com>
2026-03-22 03:06:13 +0700

    refactor: rename and update theme persistence methods in theme_controller

--- lib/app/core/controller/theme_controller.dart
+++ lib/app/core/controller/theme_controller.dart
@@ -21,23 +21,27 @@ class ThemeController extends GetxController {
   final _themeKey = "isDarkTheme";
 
   /// Get isDarkMode info from local storage and return ThemeMode
-  ThemeMode get theme => _loadThemeFromStorage() ?? false ? ThemeMode.dark : ThemeMode.light;
+  ThemeMode get theme => loadThemeMode() ? ThemeMode.dark : ThemeMode.light;
 
-  /// Load isDArkMode from local storage and if it's empty, returns false (that means default theme is light)
-  bool? _loadThemeFromStorage() => sharedPreferencesManager.getBool(_themeKey);
+  /// Load isDarkMode from local storage and if it's empty, returns false (that means default theme is light)
+  bool loadThemeMode() {
+    return sharedPreferencesManager.getBool(_themeKey);
+  }
 
   /// Save isDarkMode to local storage
-  void _saveThemeToStorage(bool isDarkTheme) => sharedPreferencesManager.putBool(_themeKey, isDarkTheme);
+  Future<void> saveThemeMode(bool isDarkMode) async {
+    await sharedPreferencesManager.putBool(_themeKey, isDarkMode);
+  }
 
   /// Switch theme and save to local storage
   Future<void> switchTheme(bool themeValue) async {
     if (themeValue) {
-      _saveThemeToStorage(themeValue);
+      saveThemeMode(themeValue);
       appController.isDarkTheme.value = themeValue;
       appController.updateTheme(AppTheme.fromType(ThemeType.dark));
       Get.changeThemeMode(ThemeMode.dark);
     } else {
-      _saveThemeToStorage(themeValue);
+      saveThemeMode(themeValue);
       appController.isDarkTheme.value = themeValue;
       appController.updateTheme(AppTheme.fromType(ThemeType.light));
       Get.changeThemeMode(ThemeMode.light);
