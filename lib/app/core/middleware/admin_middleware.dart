e1da6fb4372670295ee9b8d1c5dd7e882f436e5d
vinsensiusarko <vinsensiusarka@gmail.com>
2026-03-22 14:04:34 +0700

    feature: add admin middleware class

---
+++ lib/app/core/middleware/admin_middleware.dart
@@ -0,0 +1,11 @@
+/*
+ * Created by Vinsensius Arko on 22/03/2026, 13:48
+ * vinsensiusarka@gmail.com
+ * Last modified 22/03/2026, 13:48
+ * Copyright (c) 2026.
+ * All rights reserved.
+ */
+
+import 'package:get/get.dart';
+
+class AdminMiddleware extends GetMiddleware {}
