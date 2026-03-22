418bf27b1aac37d0a63c3a0d90dc16b2303bc84a
vinsensiusarko <vinsensiusarka@gmail.com>
2026-03-22 14:04:57 +0700

    refactor: add animation controller, loading state, and data refresh logic to home controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,44 @@
+import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with GetSingleTickerProviderStateMixin {
+  bool hasNotification = true;
 
-  final String title = 'Test Home Controller';
+  late AnimationController animController;
+  var isLoading = false.obs;
+
+  String get title => 'Home Controller';
+
+  Future<void> refreshData() async {
+    if (isLoading.value) return;
+
+    isLoading.value = true;
+    animController.repeat();
+    update();
+
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+    } finally {
+      await animController.animateTo(1.0, curve: Curves.linear);
+
+      animController.reset();
+      isLoading.value = false;
+      update();
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    animController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+  }
+
+  @override
+  void onClose() {
+    animController.dispose();
+    super.onClose();
+  }
 }
