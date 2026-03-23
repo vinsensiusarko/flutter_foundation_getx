28f47d1554eeaa87603c5f347fc597b5f2c1acab
vinsensiusarko <vinsensiusarka@gmail.com>
2026-03-24 05:16:53 +0700

    refactor: update design and functionality of custom snackbar widget

--- lib/app/widget/custom_snackbar.dart
+++ lib/app/widget/custom_snackbar.dart
@@ -1,61 +1,128 @@
 /*
- * Created by Vinsensius Arko on 24/03/2026, 04:44
+ * Created by Vinsensius Arko on 08/12/2024, 05:38
  * vinsensiusarka@gmail.com
- * Last modified 18/07/2025, 23:03
- * Copyright (c) 2026.
+ * Last modified 06/12/2024, 20:58
+ * Copyright (c) 2024.
  * All rights reserved.
  */
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_foundation_getx/app/core/util/dimensions.dart';
-import 'package:flutter_foundation_getx/style/color.dart';
 import 'package:get/get.dart';
 
-import 'custom_icon_card.dart';
+import '../core/util/dimensions.dart';
 
-void showCustomSnackBar(String message, {bool isError = true, String title = "Gagal"}) {
-  if (!Get.isSnackbarOpen) {
-    Get.snackbar(
-      title,
-      message,
-      titleText: Text(
-        title,
-        style: TextStyle(
-          color: AppColors.colorWhite,
-          fontWeight: FontWeight.w400,
-          fontSize: Dimensions.font15
+SnackbarController? controller;
+
+Future<void> showCustomSnackBar({String title = 'Gagal', bool isError = true, required String message, String? messageWithButton, Function? onTap}) async {
+  Get.closeAllSnackbars();
+  controller = Get.showSnackbar(
+    GetSnackBar(
+      boxShadows: [
+        if (isError)...[
+          BoxShadow(
+            color: Color(0xFFE5A5A5).withValues(alpha: 0.05),
+            blurRadius: 6,
+            spreadRadius: 4,
+            offset: const Offset(2, 2),
+          ),
+        ] else...[
+          BoxShadow(
+            color: Color(0xFFA5E5B3).withAlpha(13),
+            blurRadius: 6,
+            spreadRadius: 4,
+            offset: const Offset(2, 2)
+          ),
+        ]
+      ],
+      titleText: ListTile(
+        contentPadding: EdgeInsets.zero,
+        titleAlignment: ListTileTitleAlignment.top,
+        minTileHeight: 0,
+        minVerticalPadding: 0,
+        leading: Builder(
+          builder: (context) {
+            if (isError) {
+              return Icon(
+                Icons.error,
+                color: Colors.red,
+                size: Dimensions.radius20,
+              );
+            } else {
+              return Icon(
+                Icons.check_circle,
+                color: Colors.green,
+                size: Dimensions.radius20,
+              );
+            }
+          }
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Color(0xFF1F1F1F),
+            fontSize: Dimensions.font16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 4),
+          child: Text.rich(
+            TextSpan(
+              style: TextStyle(
+                color: Color(0xFF1F1F1F),
+                fontSize: Dimensions.font14,
+              ),
+              children: [
+                TextSpan(text: '$message '),
+                TextSpan(
+                  text: messageWithButton ?? '',
+                  style: TextStyle(
+                    color: isError ? Color(0xFFFD4939) : Color(0xFF008172),
+                    fontSize: Dimensions.font14,
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap = () {
+                    if (onTap != null) {
+                      onTap();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        trailing: TextButton(
+          style: TextButton.styleFrom(
+            minimumSize: Size.zero,
+            padding: EdgeInsets.symmetric(horizontal: Dimensions.width10 - 9),
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {
+            Future.delayed(Duration.zero).then((value) {
+              if (controller != null) {
+                controller?.close(withAnimations: false);
+              }
+            });
+          },
+          child: Icon(
+            Icons.close,
+            color: Color(0xFF1F1F1F),
+            size: Dimensions.radius15 + 2
+          ),
         ),
       ),
-      messageText: Text(
-        message,
-        style: TextStyle(
-          color: AppColors.colorWhite,
-          fontWeight: FontWeight.w400,
-          fontSize: Dimensions.font12
-        ),
-      ),
-      margin: EdgeInsets.all(Dimensions.radius10 - 2),
-      backgroundColor: isError ? AppColors.colorRedAccent : AppColors.colorGreen,
-      colorText: AppColors.colorWhite,
-      snackStyle: SnackStyle.FLOATING,
+      messageText: const SizedBox(),
+      margin: EdgeInsets.only(top: Dimensions.height20 - 6),
+      borderRadius: Dimensions.radius12,
+      backgroundColor: isError ? Color(0xFFFFF5F4) : Color(0xFFF1FFF4),
+      borderColor: isError ? Color(0xFFFFBFB9) : Color(0xFFA5E5B3),
       snackPosition: SnackPosition.TOP,
-      borderRadius: Dimensions.radius10,
-      duration: const Duration(seconds: 3),
-      isDismissible: true,
-      dismissDirection: DismissDirection.up,
-      mainButton: TextButton(
-        onPressed: () {
-          Get.back();
-        },
-        child: CustomIconCard(
-          height: Dimensions.height30,
-          width: Dimensions.width30,
-          icon: Icons.close,
-          iconSize: Dimensions.radius20,
-          iconColor: AppColors.colorRed,
-          backgroundColor: AppColors.colorWhite,
-        ),
-      )
-    );
-  }
+      padding: EdgeInsets.symmetric(
+        vertical: Dimensions.height10 + 2,
+        horizontal: Dimensions.width10 + 3,
+      ),
+      maxWidth: Dimensions.width45 * 8.6,
+      duration: const Duration(milliseconds: 3000),
+    ),
+  );
 }
